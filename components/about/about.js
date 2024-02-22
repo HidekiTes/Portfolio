6f85a8ae760f909755fdba70feba80beb8b6af73
Pedro Luiz <pedro5353535@gmail.com>
2024-02-22 16:31:05 -0300

    Subida de Emergencia

---
+++ components/about/about.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("A about foi carregada");
+
+    let aboutContainer = document.getElementById("about")
+    console.log(document.getElementById("about"));
+
+    let aboutHTML = 
+        `
+            <div class="title-container">
+                <div class="title-layout">  
+                    <h1 class="h1-style">AAAAAA</h1>
+                    <h1 class="h2-style">BBBBBB</h1>
+                    <p>AAAAAAAAA</p>
+                </div>
+            </div>    
+        `
+    aboutContainer.innerHTML = aboutHTML; 
+
+    let aboutCSS = document.createElement("link");
+    aboutCSS.rel="stylesheet"
+    aboutCSS.href="/components/about/about.css"
+
+    document.head.appendChild(aboutCSS)
+});
