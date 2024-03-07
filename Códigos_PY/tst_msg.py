98f4458cb73ff4cf0bd6650c365687ba0fdea641
Pedro Luiz <pedro5353535@gmail.com>
2024-03-06 23:40:11 -0300

    Atualização de .py com Selenium

--- Códigos_PY/tst_msg.py
+++ Códigos_PY/tst_msg.py
@@ -1,57 +1,54 @@
-from pathlib import Path
-import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
+from selenium.common.exceptions import TimeoutException
 
-# Caminho para a raiz do projeto
-ROOT_FOLDER = Path(__file__).parent
-# Caminho para a pasta onde o chromedriver está
-CHROME_DRIVER_PATH = os.path.join(ROOT_FOLDER, 'drivers', 'chromedriver')
+import time
 
+# Definindo o contato e a mensagem
+contact = "Bloco de Notas"
+text = "Hey, this message was sent using Selenium"
 
-def make_chrome_browser(*options: str) -> webdriver.Chrome:
-    chrome_options = webdriver.ChromeOptions()
+# Inicializando o driver do Chrome
+driver = webdriver.Chrome()
+driver.get("https://web.whatsapp.com")
 
-    # chrome_options.add_argument('--headless')
-    if options is not None:
-        for option in options:
-            chrome_options.add_argument(option)  # type: ignore
+# Aguardando a entrada do usuário para escanear o código QR
+print("Scan QR Code, And then Enter")
+input()
 
-    chrome_service = Service(
-        executable=str(CHROME_DRIVER_PATH),
+# Informando que o usuário está logado
+print("Logged In")
+
+try:
+    # Localizando o campo de pesquisa e digitando o nome do contato
+    search_input_xpath = '//*[@id="side"]/div[1]/div/div[2]/div[2]/div'
+    search_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, search_input_xpath))
     )
+    search_input.click()
+    time.sleep(2)
+    search_input.send_keys(contact)
+    time.sleep(2)
 
-    browser = webdriver.Chrome(
-        service=chrome_service,
-        options=chrome_options
+    # Selecionando o contato desejado
+    selected_contact_css = f'span[title="{contact}"]'
+    selected_contact = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, selected_contact_css)))
+    selected_contact.click()
+
+    # Localizando o campo de mensagem e enviando a mensagem
+    message_input_xpath = '//*[@id="side"]/div[1]/div/div[2]/div[2]/div'
+    input_box = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, message_input_xpath))
     )
+    time.sleep(2)
+    input_box.send_keys(text + Keys.ENTER)
+    time.sleep(2)
 
-    return browser
+except TimeoutException:
+    print("TimeoutException: Não foi possível encontrar o elemento dentro do tempo limite.")
 
-
-if __name__ == '__main__':
-    # Example
-    # options = '--headless', '--disable-gpu',
-    options = ()
-    browser = make_chrome_browser(*options)
-
-    # Navegar para a página do WhatsApp
-    browser.get('https://web.whatsapp.com/')
-
-    # Aguardar até que o elemento do QRCode seja visível na tela
-    wait = WebDriverWait(browser, 60)  # ajuste o tempo máximo de espera conforme necessário
-    qrcode_element = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div[3]/div[1]/div/div/div[2]/div/canvas')))
-
-    # Adicionar o restante do código aqui, após o carregamento do QRCode
-    # Exemplo:
-    # realizar_login(browser)
-    # enviar_mensagem(browser, "Olá, mundo!")
-
-    # Manter o programa em execução
-    while True:
-        sleep(5)
-        # Adicionar outras tarefas aqui, se necessário
+  
